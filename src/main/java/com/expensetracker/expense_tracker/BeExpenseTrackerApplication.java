ff720e870d3fbc87a49521488331dde43f4a5442
Awais Waheed <awaiswaheed321@gmail.com>
2024-10-15 14:31:16 -0500

    Base Code Added

---
+++ src/main/java/com/expensetracker/expense_tracker/BeExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.expensetracker.expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeExpenseTrackerApplication.class, args);
+	}
+
+}
